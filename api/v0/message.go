f6e482566fc8c3e45559bd913c97fcae9184b7c8
aryanugroho <mail.anugroho@gmail.com>
2020-05-05 12:55:44 +0700

    finishing add endpoint

--- api/v0/message.go
+++ api/v0/message.go
@@ -1,9 +1,25 @@
 package apiv0
 
-import "net/http"
+import (
+	"github.com/aryanugroho/wmessage-go/pkg"
+	"github.com/aryanugroho/wmessage-go/service"
+	"github.com/aryanugroho/wmessage-go/util"
+	"net/http"
+)
 
 func addMessage(w http.ResponseWriter, r *http.Request) {
+	var payload pkg.Message
+	err := util.BindJSON(r, &payload)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+	}
 
+	err = service.AddMessage(&payload)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	util.ToJSON(w, http.StatusCreated, `{"status": "OK"}`)
 }
 
 func listMessage(w http.ResponseWriter, r *http.Request) {
