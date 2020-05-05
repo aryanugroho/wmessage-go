02dafd75884d3de331bac394569bb49e331f50a2
aryanugroho <mail.anugroho@gmail.com>
2020-05-05 14:49:35 +0700

    adding logic to sent to web socket

--- api/v0/message.go
+++ api/v0/message.go
@@ -1,9 +1,11 @@
 package apiv0
 
 import (
+	"fmt"
 	"github.com/aryanugroho/wmessage-go/pkg"
 	"github.com/aryanugroho/wmessage-go/service"
 	"github.com/aryanugroho/wmessage-go/util"
+	"golang.org/x/net/websocket"
 	"net/http"
 )
 
@@ -19,6 +21,16 @@ func addMessage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
+	}
+
+	conn, err := websocket.Dial("ws://localhost:9000/v0/message/ws", "ws", "localhost:9000")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	if err := websocket.JSON.Send(conn, &payload); err != nil {
+		// handle error
+		fmt.Println(err.Error())
 	}
 
 	util.ToJSON(w, http.StatusCreated, `{"status": "OK"}`)
